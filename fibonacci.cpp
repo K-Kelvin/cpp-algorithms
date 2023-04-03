1126d6b20e970a19b413ad3dbdb6111795c1a38e
K-Kelvin <kelvinkijanda@gmail.com>
2023-04-03 21:32:55 +0300

    Made fibonacci program dynamic, to accept 1 or 2 inputs

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -10,26 +10,40 @@ int fibonacci(int n) {
 }
 
 // Print a fibonacci sequence of the length provided
-void printSequence(int noOfTerms) {
-    for (int i = 1; i <= noOfTerms; ++i) {
+void printSequence(int start, int stop) {
+    for (int i = start; i <= stop; ++i) {
         cout << fibonacci(i) << " ";
     }
     cout << "\n";
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
+    if (argc < 2 || argc > 3) {
         cout << "Usage: " << endl;
-        cout << "     " << argv[0] << " [length]\n" << endl;
+        cout << "     " << argv[0] << " [start] [stop]" << endl;
+        cout << "  or " << argv[0] << " [stop]\n" << endl;
         cout << "Examples: " << endl;
-        cout << "     " << argv[0] << " 6" << endl;
+        cout << "     " << argv[0] << " 1 6" << endl;
         cout << "     " << argv[0] << " 10" << endl;
         return 1;
     }
 
-    int length = atoi(argv[1]); // get the second argument provided
+    int start, stop;
+    if (argc == 2) {
+        start = 0; // get the second argument provided
+        stop = atoi(argv[1]); // get the third argument provided
+    } else if (argc == 3) {
+        start = atoi(argv[1]);
+        stop = atoi(argv[2]);
+    }
 
-    cout << "-------- Fibonacci Sequence [length=" << length << "]--------" << endl;
-    printSequence(length);
+    // validate input
+    if (start < 0 || stop < 0) {
+        cout << "Only positive ranges allowed! Exiting...\n\n";
+        return 1;
+    }
+
+    cout << "-------- Fibonacci Sequence [" << start << "-" << stop << "]--------" << endl;
+    printSequence(start, stop);
     return 0;
 }
